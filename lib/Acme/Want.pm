3c70d62c7aa1b2b21215c7547719ea0db94e4b30
tokuhirom <tokuhirom@gmail.com>
2010-09-11 15:02:31 +0900

    initial import

---
+++ lib/Acme/Want.pm
@@ -0,0 +1,39 @@
+package Acme::Want;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Acme::Want -
+
+=head1 SYNOPSIS
+
+  use Acme::Want;
+
+=head1 DESCRIPTION
+
+Acme::Want is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
